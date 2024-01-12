36bc1c1a0e753fdee29b3ee1e00b19703ae2672a
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-12 01:17:46 +0000

    Implementation of lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" """
+
+
+def canUnlockAll(boxes):
+    """ """
+    opened_boxes = {0}
+
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+
+        for key in boxes[current_box]:
+            if key not in opened_boxes:
+                opened_boxes.add(key)
+                queue.append(key)
+
+    return len(opened_boxes) == len(boxes)
