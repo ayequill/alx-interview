a05d40f804112a49ee13cc92d968f18f0bfc3efd
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-27 00:47:22 +0000

    refactored

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -20,8 +20,8 @@ if __name__ == '__main__':
                 line_parts = line.split()
 
                 total_file_size += int(line_parts[-1])
-                status = line_parts[-2]
-                statuses = ["200", "301", "400", "401", "403", "404", "405", "500"]
+                status = int(line_parts[-2])
+                statuses = [200, 301, 400, 401, 403, 404, 405, 500]
                 if status in statuses:
                     if status in codes:
                         codes[status] += 1
