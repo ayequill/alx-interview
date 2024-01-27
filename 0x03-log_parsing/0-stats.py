2628d0f3def62ff1faa6d85b96bacf20789444da
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-01-27 00:18:13 +0000

    refactored

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,13 +8,11 @@ if __name__ == '__main__':
 
     line_counts = 0
 
-
     def help_print():
         """ Helper Function to print """
         print(f"File size: {total_file_size}")
         for code, count in sorted(codes.items()):
             print(f"{code}: {count}")
-
 
     try:
         for line in stdin:
@@ -22,14 +20,14 @@ if __name__ == '__main__':
                 line_parts = line.split()
 
                 total_file_size += int(line_parts[-1])
-                # if line_parts[-2] in [200, 301, 400, 401, 403, 404, 405, 500]:
-                status = line_parts[-2]
+                if line_parts[-2] in [200, 301, 400, 401, 403, 404, 405, 500]:
+                    status = line_parts[-2]
 
-                if status in codes:
-                    codes[status] += 1
-                else:
-                    codes[status] = 1
-                line_counts += 1
+                    if status in codes:
+                        codes[status] += 1
+                    else:
+                        codes[status] = 1
+                    line_counts += 1
 
             except (ValueError, IndexError):
                 continue
